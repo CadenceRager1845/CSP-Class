eb5c605e1223f2f0df824f74588b3d360fe4df40
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-09-11 20:04:34 +0000

    added variables to variables practice

--- Variables/variables practice.c
+++ Variables/variables practice.c
@@ -10,5 +10,23 @@ int main(void) {
     char number[ ] = "999"; 
     printf("%s",number); 
 
-    char breakfast[ ] = "Cereal"
-             }
+    char breakfast[ ] = "Cereal";
+    printf("%s",breakfast);
+
+    char name[ ] = "UCAS";
+    printf("%s",name);
+
+    char number[ ] = "2024";
+    printf("%s",number);
+
+    char color[ ] = "Cat Eyes";
+    printf("%s",color);
+
+    char number[ ] = "14";
+    printf("%s",number);
+
+    char subject[ ] = "English"
+    printf("%s",number); 
+
+    
+                 }
