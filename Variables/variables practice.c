89cfa955b255050d47f48b09f2475ff9527e9de4
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-09-11 19:37:59 +0000

    adding variables to variables practice.c

---
+++ Variables/variables practice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[ ] = "Cadence";
+    printf("%s",name); 
+
+    char number[ ] = "3";
+    printf("%s",number); 
+
+    char number[ ] = "999"; 
+    printf("%s",number); 
+
+    char breakfast[ ] = "Cereal"
+             }
