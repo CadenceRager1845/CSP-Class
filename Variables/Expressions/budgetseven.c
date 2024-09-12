7aca849ca179332ae7adfa4b080ee9ae08e65666
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-09-12 20:39:13 +0000

    added more variables to budgetseven.c

--- Variables/Expressions/budgetseven.c
+++ Variables/Expressions/budgetseven.c
@@ -2,24 +2,32 @@
 
 int main(void) {
     float income, rent, utilities, groceries, transportation, expenses, savings, total;
+    float prent, putilities, pgroceries, ptransportation, pexpenses; 
     printf("This is going to calculate your budget for the month.");
     printf("How much do you make in a month?\n");
     scanf("%f", &income);
     scanf("Your income is: $%.2f\n", income);
     printf("How much is rent?\n");
-    scanf("%f",&rent)
+    scanf("%f",&rent);
     printf("How much is utilities?\n");
     scanf("%f",&utilities);
     printf("How much is groceries?\n");
     scanf("%f", &groceries);
     printf("How much is transportation\n");
     scanf("%f", &transportation);
-    expenses = rent + utilities + groceries + transportation 
+    expenses = rent + utilities + groceries + transportation;
     savings = income *.2;
     total = income - expenses - savings;
+    prent = rent/income; 
+    putilities = untilities/income;
+    ptransportation = transportation/income; 
+    pexpenses = expenses/income; 
     printf("Your income is: $%.2f\n", income);
     printf("Your expenses are: $%.2f\n", expenses);
-    printf("Your savings are: $%.2f\n", saving); 
+    printf("Your savings are: $%.2f\n", savings); 
     printf("Your total left to spend is: $%.2f\n", total);
+    printf("Your rent is %.2f", prent, "of your income"); 
+    printf("Your rent is %.2f", putilities, "of your income"); 
+    printf("Your rent is %.2f", ptransportation, "of your income"); 
     return 0;
 }
