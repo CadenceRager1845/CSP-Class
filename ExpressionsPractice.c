b299cb73973c6aa8053a99416d8b4280017fa0a3
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-09-18 20:35:00 +0000

    added variables to ExpressionsPractice.c

--- ExpressionsPractice.c
+++ ExpressionsPractice.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <math.h> 
 
 int main(void) {
-    printf("%f", 7-24\8*4+6);
-    printf("%f", 18\3-7+2*5);
-    printf("%f", 6*4\12+72\8-9);
-    printf("%f", (17-6\2)+4*3);
-    printf("%f", -2(1*4-2\2)+(6+2-3));
-    printf("%f", -1*[(3-4*7)\5]-2*24\6);
-    printf("%f", (3*10\15)-(5-4));
-    printf("%f", (4*4+9)-10\4);
-    printf("%f", (22\2-2*5)+(4-6\6));
+    printf("%d\n", 7-24/8*4+6);
+    printf("%d\n", 18/3-7+2*5);
+    printf("%d\n", 6*4/12+72/8-9);
+    printf("%d\n", (17-6/2)+4*3);
+    printf("%d\n", -2*(1*4-2/2)+(6+2-3));
+    printf("%d\n", -1*((3-4*7)/5)-2*24/6);
+    printf("%d\n", (3*(int) pow(5,2)/15-(5-(int) pow(2,2))));
+    printf("%d\n", ((int) pow(1,4)*(int) pow(2,2)+(int) pow(3,3))-((int) pow(2,5)/4));
+    printf("%d\n", (22/2-2*5)(int) pow(2)+(4-6/6)(int) pow(2));
     return 0;
 }
