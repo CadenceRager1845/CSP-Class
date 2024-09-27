1000acf5b94fd6245fed4be950d8c9caba8bf541
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-09-27 20:41:28 +0000

    adfskhu

---
+++ Functions/update_helloworld.py
@@ -0,0 +1,6 @@
+#Make a function that allows them to input a name
+def name():
+    return input("What is your name?:" )
+#make a function that prints their name 
+def nameCreate()
+#use the funtions together to make your code work
