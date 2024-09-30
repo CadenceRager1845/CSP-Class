7cd57c1f56ea7b6d19146e74b742c201fcb58a40
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-09-30 19:08:56 +0000

    ;wieuh

--- namedecorator/namedecorator.c
+++ namedecorator/namedecorator.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <cstring> 
 
 int main(void) {
-    char name[30]; 
-    printf("  ");
+    char name[20], sentence[20]; 
+    printf("Type a name: ");
+    scanf("%s", &name); 
+    strcat(sentence, name); 
+    printf(":0 :0 %s :0 :0", name);
     return 0;
     }
