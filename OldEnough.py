d477e6ce2742d0b8ca94b1591731a407866fcaa2
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-01 20:13:36 +0000

    wef

---
+++ OldEnough.py
@@ -0,0 +1,12 @@
+age = 18
+voter = "yes"
+if age >= 18 and voter == "yes": 
+    print("You are able to vote!")
+elif age >= 16: 
+    print("You are able to drive!")
+elif age >= 15:
+    print("You are able to get a learners permit!")
+elif age >= 5:
+    print("You are are old enough to go to school!")
+else: 
+    print("You are too young to go to school...") 
