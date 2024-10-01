33910c66b342f3855f1162cf1b09efb46b500afa
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-01 20:23:59 +0000

    asdfghjkl

---
+++ OldEnough.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+int age =20; 
+bool vote = false;
+int main(void){
+    if (age >= 18 vote == true){ 
+        printf("You are old enough to vote!\n");
+    }else if (age >= 16){
+        printf("You are old enough to drive!\n");
+    }else if (age == 15){
+        printf("You are old enough to get a learners permit!");
+    }else if (age >= 5){
+        printf("You are old enough to go to school!");
+    }else{
+        printf("Sorry, you are too young to go to school...");
+    }
+    return 0; 
+}
