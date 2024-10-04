897185cc75f82ecd03cab9b124ec7ba848f4fb6b
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-04 20:24:22 +0000

    asdf

--- Functions/update_helloworld.c
+++ Functions/update_helloworld.c
@@ -4,5 +4,10 @@ void names (char name[]){
     printf("Hello %s", name); 
 }
 int main(void){
-    names("Amy"); 
-}
+    names("Amy!"); 
+    names("Sierra!");
+    names("James!"); 
+    names("Aaron!"); 
+    names("Juan!"); 
+}  
+     
