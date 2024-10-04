1d6bf7178b66a27ef5d84e7b54a71447cc92be12
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-04 20:30:03 +0000

    asdf

--- Variables/variables practice.py
+++ Variables/variables practice.py
@@ -1,10 +1,20 @@
-name = print("Cadence")
-number = print("3")
-number = print("999")
-breakfast = print("Cereal")
-color = print("Pink and Blue")
-name = print("UCAS")
-year = print("2024")
-color = print("cat eyes")
-age = print("14")
-subject = print("english")
+name = "Cadence"
+print(name) 
+number = "3"
+print(number)
+number = "999"
+print(number)
+breakfast = "Cereal"
+print(breakfast)
+color = "Pink and Blue"
+print(color) 
+name = "UCAS"
+print(name) 
+year = "2024"
+print(year)
+color = "Cat Eyes"
+print(color) 
+age = "14"
+print(age) 
+subject = "English"
+print(subject) 
