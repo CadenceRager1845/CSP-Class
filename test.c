114b207902244676e711442fdf5ed13024d4f305
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-07 20:41:47 +0000

    asdf

--- test.c
+++ test.c
@@ -1,9 +1,10 @@
 #include <stdio.h> 
-char siblings[8][20] = {"Judah", "Amanda", "Joshua", "William", "Jennifer", "Lady", "Margo", "Pepper"}; 
+char siblings[8][20] = {"Judah", "Amanda", "Joshua", "William", "Jennifer", "Lady", "Margo", "Pepper",}; 
 int i; 
 int main (){
     while (i < 8){
-        printf("%s Rager", siblings[i]); 
+        printf("%s Rager\n", siblings[i]); 
+        i+=1; 
     }
     return 0; 
 }
