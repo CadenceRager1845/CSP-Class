5abdd57acab7488a06fd59a554bd0e611a599757
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-07 19:11:04 +0000

    asdf

--- Functions/update_helloworld.py
+++ Functions/update_helloworld.py
@@ -2,20 +2,10 @@ def names(input):
     return f"Hello {input}"
 print(names("Amy!")) 
 
-def names(input):
-    return f"Hello {input}"
 print(names("Sierra!"))
 
-def names(input):
-    return f"Hello {input}"
 print(names("James!"))
 
-def names(input):
-    return f"Hello {input}"
 print(names("Aaron!"))
 
-def names(input):
-    return f"Hello {input}"
-print(names("Juan!"))  
-
-
+print(names("Juan!"))
