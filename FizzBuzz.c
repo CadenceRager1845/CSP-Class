77ee7106527611ea7cba618ac65dcd828451f855
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-10 20:42:00 +0000

    asdfasdf

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
+#include <string.h> 
+#include <time.h> 
+int i, usr; 
+char one[50], two[50], three[50]; 
+
+void delay(int sec){
+    int milli = 1000*sec;
+    clock_t start = clock(); 
+
+    while(clock()< start+milli); 
+}
+
 
 int main(){
+    printf("Give me a number: \n"); 
+    scanf("%d", &usr); 
+    printf("Give me a word: \n"); 
+    scanf("%s", one);
+    printf("Give me a another word: \n"); 
+    scanf("%s", two);\
+    strcat(three, one); 
+    strcat(three, two); 
     //create a loop that counts to 50
-    
-    //replace #'s divisable by 3 with "Fizz" 
-
+    for(i=1;i<=usr;i++){
+    //replace #'s divisible by both with "FizzBuzz"
+    if(i%3==0 && i%5==0){
+        printf("%s\n", three); 
+    }else if (i%3==0){
+        printf("%s\n", one);  
     //replace #'s divisble by 5 with "Buzz" 
-
-    //replace #'s divisible by both with "FizzBuzz" 
-
-    //print all other numbers 
-    printf("Hello World!\n"); 
+    }else if (i%5==0){
+        printf("%s\n", two); 
+    //print all other numbers
+    }else{
+        printf("%d\n", i);
+            }
+            delay(10000); 
+        }
     return 0; 
-}
+} 
