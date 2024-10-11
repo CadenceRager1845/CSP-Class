d6af5d1fa35c43b27ebcd8e918735c9702079dd0
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-11 20:12:42 +0000

    asdfasdf

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,7 @@
 #Build a loop that counts to 50
 
-#Create a function 
+#Create a function that replaces numbers divisible by 3 with "Fizz"
+
+#Create a function that replaces numbers divisible by 5 with "Buzz"
+
+#Create a function that replaces numbers divisible by 3 and 5 with "FizzBuzz"
