f1e3fddb783e0534e18e42bc0397b51fcaf0ee53
CadenceRager1845 <cadence.rager@ucas-edu.net>
2024-10-11 20:41:19 +0000

    asdf

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,17 @@
 #Build a loop that counts to 50
-
+i = 1
+while i<=50: 
+#Create a function that replaces numbers divisible by 3 and 5 with "FizzBuzz"
+    if i%3==0 and i%5 == 0:
+      print("FizzBuzz")
+    elif i%5 == 0:
+        print("Buzz")
+    elif i%3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+    
+   
 #Create a function that replaces numbers divisible by 3 with "Fizz"
 
 #Create a function that replaces numbers divisible by 5 with "Buzz"
-
-#Create a function that replaces numbers divisible by 3 and 5 with "FizzBuzz"
